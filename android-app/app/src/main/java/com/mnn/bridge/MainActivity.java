1a606bbea91d503b22db8b3e56d65fd75db3d7d3
Dex383 <dextrox383@gmail.com>
2026-05-28 00:00:06 -0400

    fix: implement MANAGE_EXTERNAL_STORAGE for Android 11+ scoped storage

--- android-app/app/src/main/java/com/mnn/bridge/MainActivity.java
+++ android-app/app/src/main/java/com/mnn/bridge/MainActivity.java
@@ -1,9 +1,13 @@
 package com.mnn.bridge;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.Settings;
 import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -11,12 +15,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            if (!Environment.isExternalStorageManager()) {
+                Toast.makeText(this, "Please grant 'All Files Access' permission in settings", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
+                intent.setData(Uri.parse("package:" + getPackageName()));
+                startActivity(intent);
+            }
+        }
+
         // Start the Foreground Service
-        Intent intent = new Intent(this, MNNBridgeService.class);
+        Intent serviceIntent = new Intent(this, MNNBridgeService.class);
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            startForegroundService(intent);
+            startForegroundService(serviceIntent);
         } else {
-            startService(intent);
+            startService(serviceIntent);
         }
         
         Log.d("MainActivity", "Bridge service started");
