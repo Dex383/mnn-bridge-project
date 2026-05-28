0d247cd486e2565d6f2ed90f774845cf003a458f
Dex383 <dextrox383@gmail.com>
2026-05-28 00:01:14 -0400

    fix: add missing Build import in MainActivity

--- android-app/app/src/main/java/com/mnn/bridge/MainActivity.java
+++ android-app/app/src/main/java/com/mnn/bridge/MainActivity.java
@@ -2,6 +2,7 @@ package com.mnn.bridge;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.Settings;
